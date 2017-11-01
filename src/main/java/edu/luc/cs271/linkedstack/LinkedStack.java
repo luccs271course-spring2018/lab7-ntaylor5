a0ebd14cff8a5c4b4f87c22b393b88b503afd7c3
Nick Taylor <31541603+ntaylor5@users.noreply.github.com>
2017-10-31 20:26:25 -0500

    Finished Lab

--- src/main/java/edu/luc/cs271/linkedstack/LinkedStack.java
+++ src/main/java/edu/luc/cs271/linkedstack/LinkedStack.java
@@ -2,43 +2,84 @@ package edu.luc.cs271.linkedstack;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
 
 
 public class LinkedStack<E> implements IStack<E> {
 
   /** The topmost node of this stack. The stack gets pushed down from here. */
   private Node<E> top;
+  private int size;
 
   // TODO why don't we need an explicit constructor?
 
   @Override
   public E push(final E obj) {
-    // TODO
-    return null;
+    // TODO FINISHED
+    top = new Node<>(obj, top);
+    return obj;
   }
 
   @Override
   public E peek() {
-    // TODO
-    return null;
+    // TODO FINISHED
+    if (isEmpty()) {
+      throw new NoSuchElementException();
+    }
+   return top.data;
   }
 
   @Override
   public E pop() {
-    // TODO
-    return null;
+    // TODO FINISHED
+    if (isEmpty()) {
+      throw new NoSuchElementException();
+    }
+    Node<E> store = top;
+    top = top.next;
+    return store.data;
   }
 
   @Override
   public boolean isEmpty() {
-    // TODO
-    return false;
+    // TODO FINISHED
+    if (top == null) {
+      return true;
+    }
+    else {
+      return false;
+    }
   }
 
   @Override
   public List<E> asList() {
-    // TODO implement using an ArrayList preallocated with the right size
-    // TODO add any instance variable(s) required to support this
-    return null;
+    final ArrayList<E> result = new ArrayList<>(size);
+    populateList(top, result); // TODO FINISHED replace null with the right reference
+    return result;
+  }
+
+  private void populateList(final Node<E> curr, final List<E> result) {
+    // TODO FINISHED recursively populate the list in the desired order
+    if (curr == null) {
+      return;
+    }
+    result.add(curr.data);
+    populateList(curr.next, result);
+  }
+
+  @Override
+  public List<E> asFifoList() {
+    final ArrayList<E> result = new ArrayList<>(size);
+    populateFifoList(top, result); // TODO FINISHED replace null with the right reference
+    return result;
+  }
+
+  private void populateFifoList(final Node<E> curr, final List<E> result) {
+    // TODO FINISHED recursively populate the list in the desired order
+    if (curr == null) {
+      return;
+    }
+    result.add(0, curr.data);
+    populateFifoList(curr.next, result);
   }
 }
--- src/main/java/edu/luc/cs271/linkedstack/ReverseLines.java
+++ src/main/java/edu/luc/cs271/linkedstack/ReverseLines.java
@@ -1,23 +1,29 @@
 package edu.luc.cs271.linkedstack;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class ReverseLines {
 
   public static void main(final String[] args) {
-    // TODO read successive input lines until EOF, then print out in reverse order
-
-
-
+    System.out.println("Enter input, enter nothing to end.");
     final Scanner input = new Scanner(System.in);
-    while (input.hasNextLine()) {
-      final String line = input.nextLine();
+    printReverse(input);
+  }
 
-
-
+  private static void printReverse(final Scanner input) {
+    // TODO recursively read and print successive input lines until EOF, then print them out in reverse order
+    String inputString = input.nextLine();
+    Stack<String> backwards = new Stack<>();
+    if (!inputString.equals(""))
+    {
+      backwards.push(inputString);
+      System.out.println(inputString);
+      printReverse(input);
     }
-
-
-
+    else {
+      return;
+    }
+    System.out.println(backwards.pop());
   }
 }
